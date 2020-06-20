dfd8c3bcbb4763f7284a6aa246ff9f604af3df1b
Orkhan <sherisl2017@gmail.com>
2020-06-20 14:36:03 +0700

    update

--- mail.php
+++ mail.php
@@ -11,14 +11,14 @@ $email = $_POST['user_email'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'ssogroup2017@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Username = 'sso.group@inbox.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = '$dk820&7372'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('ssogroup2017@gmail.com'); // от кого будет уходить письмо?
+$mail->setFrom('sso.group@inbox.ru'); // от кого будет уходить письмо?
 $mail->addAddress('sherisl2017@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
